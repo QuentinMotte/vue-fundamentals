40f58a42e387919e9fbb195cdef4a03917b38d07
QuentinMotte <99801733+QuentinMotte@users.noreply.github.com>
2023-04-07 15:21:05 +0200

    advanced concept vue components

---
+++ components/src/components/Greeting.vue
@@ -0,0 +1,22 @@
+<template>
+  <p v-if="age >= 25">{{ msg }}</p>
+  <p v-else>You must be 25years or older to view this message</p>
+</template>
+
+<script>
+export default {
+  name: "Greeting",
+  props: ["age"],
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+p:hover {
+  color: darken($color: #cc4444, $amount: 15%);
+}
+</style>
---
+++ components/src/components/User.vue
@@ -0,0 +1,33 @@
+<template>
+  <button type="button" @click="onClickAge">Update Age Event</button>
+  <button type="button" @click="ageChangeFN(3)">Update Age CB</button>
+  <p>The user is {{ age }} years old</p>
+  <p>{{ ageDouble }}</p>
+</template>
+<script>
+export default {
+  name: "User",
+  props: {
+    age: {
+      type: Number,
+      required: true,
+      // default: 20,
+      // validator(value) {
+      //   return value < 130;
+      // },
+    },
+    ageChangeFN: Function,
+  },
+  emits: ["age-change"],
+  computed: {
+    ageDouble() {
+      return this.age * 2;
+    },
+  },
+  methods: {
+    onClickAge() {
+      this.$emit("age-change", 3);
+    },
+  },
+};
+</script>
